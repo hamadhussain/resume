8cda5239073ea5160fc6539634f2785398b1b3e9
hamadhussain <108784635+hamadhussain@users.noreply.github.com>
2024-02-27 22:00:56 +0500

    fourth

--- src/app/Components/FrontPage/page.jsx
+++ src/app/Components/FrontPage/page.jsx
@@ -71,33 +71,34 @@ const Page = () => {
       </motion.div>
       {ex && (
         <>
-          <div
-            data-aos="fade-left"
-            data-aos-anchor="#example-anchor"
-            data-aos-offset="500"
-            data-aos-duration="5000"
-            className="text-gray-400 font-serif text-xs p-10 text-center italic sd"
-          >
-            Hey there! I&apos;m a passionate undergraduate student majoring in
-            software engineering, with a keen interest in crafting seamless
-            experiences for users across web and mobile platforms. Over the last
-            couple of years, I&apos;ve immersed myself in the world of web and
-            app development, exploring various technologies and methodologies to
-            enhance my skills. Whether it&apos;s building sleek websites or
-            crafting intuitive mobile applications, I&apos;ve dedicated myself
-            to mastering the craft through freelance projects and personal
-            endeavors. Along the way, I&apos;ve had the privilege of
-            collaborating with talented teams, learning the importance of
-            effective communication and teamwork in delivering top-notch
-            solutions. Now, I&apos;m excited to take the next step in my journey
-            by pursuing internship or job opportunities where I can learn from
-            experienced developers, contribute meaningfully to projects, and
-            continue to grow both personally and professionally. With a strong
-            foundation in software engineering and a thirst for knowledge,
-            I&apos;m ready to tackle new challenges and make a positive impact
-            in the field. Let&apos;s connect and build something awesome
-            together!
-          </div>
+<div
+  data-aos="fade-left"
+  data-aos-anchor="#example-anchor"
+  data-aos-offset="500"
+  data-aos-duration="5000"
+  className="text-gray-400 font-serif text-xs p-10 text-center italic sd"
+>
+  Hey there! I&apos;m a passionate undergraduate student majoring in
+  software engineering, with a keen interest in crafting seamless
+  experiences for users across web and mobile platforms. Over the last
+  couple of years, I&apos;ve immersed myself in the world of web and
+  app development, exploring various technologies and methodologies to
+  enhance my skills. Whether it&apos;s building sleek websites or
+  crafting intuitive mobile applications, I&apos;ve dedicated myself
+  to mastering the craft through freelance projects and personal
+  endeavors. Along the way, I&apos;ve had the privilege of
+  collaborating with talented teams, learning the importance of
+  effective communication and teamwork in delivering top-notch
+  solutions. Now, I&apos;m excited to take the next step in my journey
+  by pursuing internship or job opportunities where I can learn from
+  experienced developers, contribute meaningfully to projects, and
+  continue to grow both personally and professionally. With a strong
+  foundation in software engineering and a thirst for knowledge,
+  I&apos;m ready to tackle new challenges and make a positive impact
+  in the field. Let&apos;s connect and build something awesome
+  together!
+</div>
+
 
           <center>
             {" "}
